25422eedcbd0b877356625c0bdc599c47ceaa444
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2023-02-23 16:00:54 +0700

    Fixing bug in MyWorks

--- frontend/src/views/MyWorks.js
+++ frontend/src/views/MyWorks.js
@@ -1,6 +1,7 @@
 import '../assets/Wrapper1.css'
 import '../assets/Wrapper1Anime.css'
 import '../assets/Wrapper.css'
+import '../assets/Wrapper1Anime.css'
 import '../assets/Wrapper2.css'
 import Demo from '../assets/demo-tynker.mp4'
 import LogoTynker from '../assets/logo.png'
@@ -51,7 +52,7 @@ export default function MyWorks() {
                                 <h2 data-aos="slide-right" className='worktitle'>Tynker</h2>
                             </div>
                             <div className='col-9' style={{ textAlign: 'left', marginLeft: '0px' }}>
-                                <img data-aos="slide-left" src={LogoTynker} style={{ height: '10vw', position: 'relative', zIndex: '100' }} alt='' />
+                                <img data-aos="slide-left" src={LogoTynker} style={{ height: '10vw' }} alt='' />
                             </div>
                         </div>
                         <div className='d-flex col' style={{ textAlign: 'left' }}>
@@ -99,7 +100,7 @@ export default function MyWorks() {
                     </div>
                 </div>
                 <div className="custom-shape-divider-top-1676191786" style={{ position: 'relative', margin: '0', backgroundColor: '#B6EADA', zIndex: '-1' }} id='movinganimation'>
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <svg className='movinganimation' data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                     </svg>
                 </div>
@@ -157,7 +158,7 @@ export default function MyWorks() {
                     </div>
                 </div>
                 <div className="custom-shape-divider-top-1676192244" style={{ position: 'relative', margin: '0', backgroundColor: '#03001C', zIndex: '-1' }}>
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <svg className='movinganimation' data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                     </svg>
                 </div>

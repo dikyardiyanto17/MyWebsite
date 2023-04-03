48add734c119e89df995e63d1776f1d2aba9bdf8
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2023-04-03 08:46:41 +0700

    Adding footer link

--- frontend/src/components/Footbar.js
+++ frontend/src/components/Footbar.js
@@ -16,25 +16,25 @@ export default function Footbar() {
           </div>
           <div className="d-flex flex-row-reverse align-items-center">
             <div className='footer-custom'>
-              <a style={{ textDecoration: "none" }}>
+              <a style={{ textDecoration: "none", color: '#B6EADA' }} href="https://api.whatsapp.com/send?phone=6285213030871&text=Hi Diky" target="_blank">
                 <img src={WhatsApp} className="img-custom" alt="" />
                 <span>+62 852-1303-0871</span>
               </a>
             </div>
             <div className='footer-custom'>
-              <a style={{ textDecoration: "none" }}>
+              <a style={{ textDecoration: "none", color: '#B6EADA' }} href="mailto:dikyardiyanto17@gmail.com" target="_blank">
                 <img src={Email} className="img-custom" alt="" />
                 dikyardiyanto17@gmail.com
               </a>
             </div>
             <div className='footer-custom'>
-              <a style={{ textDecoration: "none" }}>
+              <a style={{ textDecoration: "none", color: '#B6EADA' }} href="https://github.com/dikyardiyanto17" target="_blank">
                 <img src={Github} className="img-custom" alt="" />
                 Github
               </a>
             </div>
             <div className='footer-custom'>
-              <a style={{ textDecoration: "none" }}>
+              <a style={{ textDecoration: "none", color: '#B6EADA' }} href="https://www.linkedin.com/in/diky/" target="_blank">
                 <img src={Linkedin} className="img-custom" alt="" />
                 Linkedin
               </a>

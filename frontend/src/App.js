7bdce4e488c82921c87f16480ed24f4e63bf5931
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2024-08-20 17:09:17 +0700

    normal version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import router from "./routes";
 import { RouterProvider } from "react-router-dom";
-import React from 'react';
-import TagManager from 'react-gtm-module'
-const tagManagerArgs = {
-  gtmId: 'id=GTM-M3T5BT3W'
-}
-TagManager.initialize(tagManagerArgs)
+
 
 function App() {
-  window.dataLayer.push({
-    event: 'pageview'
-  });
+
   return (
       <RouterProvider router={router} />
   );
